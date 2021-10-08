96b9b714adbf4c9fff05f8592e20ef8d099aa590
erxian <evelynhsu21@gmail.com>
2021-10-08 14:54:09 -0700

    implement all function

--- hw1/EchoClient.java
+++ hw1/EchoClient.java
@@ -30,38 +30,49 @@ public class EchoClient {
       InetAddress local = InetAddress.getLocalHost();  // Can use InetAddress.getByName().
       System.out.println("LocalHost: " + local);
       Socket clientSocket = new Socket(local, SERVER_PORT);
-      //PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
       OutputStream out = clientSocket.getOutputStream();
-      //BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
       InputStream in = clientSocket.getInputStream();
       System.out.println("Connected to server " + clientSocket.getInetAddress()
                          + " port " + clientSocket.getPort());
       Scanner scanner = new Scanner(System.in);
       while (true) {
-        System.out.print("Enter text: ");
+        System.out.print("Enter whatever you want: ");
         String line = scanner.nextLine();
         if ("quit".equalsIgnoreCase(line)) {
           break;
         }
-        //writer.println(line);
-        //String response = reader.readLine();
-        //System.out.println("Server response: " + response);
-      
+
+        // sample expressions encoded according to the protocol 
       	byte[] message = new byte[] {0x00, 0x06,
-					 0x00, 0x03, 0x31, 0x2D, 0x32, 0x00, 0x0A,
-				         0x33, 0x2B, 0x34, 0x2D, 0x35, 0x2D, 0x31, 0x2B, 0x31, 0x30,
-					 0x00, 0x03, 0x31, 0x2D, 0x32, 0x00, 0x0A,
-					 0x33, 0x2B, 0x34, 0x2D, 0x35, 0x2D, 0x31, 0x2B, 0x31, 0x30,
-					 0x00, 0x03, 0x31, 0x2D, 0x32, 0x00, 0x0A,
-					 0x33, 0x2B, 0x34, 0x2D, 0x35, 0x2D, 0x31, 0x2B, 0x31, 0x30
+					 0x00, 0x03, 0x31, 0x2D, 0x32,
+                     0x00, 0x0A,
+				     0x33, 0x2B, 0x34, 0x2D, 0x35, 0x2D, 0x31, 0x2B, 0x31, 0x30,
+					 0x00, 0x03, 0x31, 0x2D, 0x32,
+                     0x00, 0x06,
+                     0x37, 0x2B, 0x31, 0x32, 0x2D, 0x33,
+					 0x00, 0x12, 
+                     0x31, 0x2B, 0x32, 0x2D, 0x33, 0x2B, 0x34, 0x2B, 0x35,
+                     0x2D, 0x39, 0x2B, 0x36, 0x2B, 0x37, 0x2D, 0x31, 0x33,
+					 0x00, 0x03, 0x30, 0x2D, 0x32 
 				    };
+        // send message to sever
       	out.write(message); 
-	
+
     	// receive from server
-        ByteArrayOutputStream response = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        response.write(buffer, 0, in.read(buffer));
-        System.out.println("Server response is:" + print(response.toByteArray()));
+        byte[] buffer = new byte[128];
+        //response.write(buffer, 0, in.read(buffer));
+        in.read(buffer, 0, 2);
+        int answerNum = ((buffer[0] & 0XFF) << 8) | ((buffer[1] & 0XFF) << 0);
+        System.out.println("there are: " + answerNum + " answers"); 
+        byte[] lenBuffer = new byte[2];
+        for (int i=0; i<answerNum; i++) {
+            in.read(lenBuffer, 0, 2);
+            int answerLen = ((lenBuffer[0] & 0XFF) << 8) | ((lenBuffer[1] & 0XFF) << 0);
+            byte[] ansBuffer = new byte[10];
+            in.read(ansBuffer, 0, answerLen);
+            String answer = new String(Arrays.copyOfRange(ansBuffer, 0, answerLen));
+            System.out.println(i+1 + " answer is: " + answer);
+        }
     }
     clientSocket.close();
     } catch (IOException e) {
--- hw1/Handler.java
+++ hw1/Handler.java
@@ -18,16 +18,15 @@ public class Handler implements Runnable {
         this.clientSocket = clientSocket;
     }
 
-
-       // this is a helper function, print an byte array
-    public static String print(byte[] bytes) {
-      StringBuilder sb = new StringBuilder();
-      sb.append("[ ");
-      for (byte b : bytes) {
-          sb.append(String.format("0x%02X ", b));
-      }
-      sb.append("]"); return sb.toString();
-    }
+    // this is a helper function, print byte array
+    //public static String print(byte[] bytes) {
+    //  StringBuilder sb = new StringBuilder();
+    //  sb.append("[ ");
+    //  for (byte b : bytes) {
+    //      sb.append(String.format("0x%02X ", b));
+    //  }
+    //  sb.append("]"); return sb.toString();
+    //}
 
     // convert the partial of an byte array to an int
     public static int byteToint(byte[] bytes, int start, int end) {
@@ -42,140 +41,102 @@ public class Handler implements Runnable {
         		(byte)value };
     }
 
-    // encode an int number to bytes and associated with the length of number
+    // encode an int number to bytes with big endian and associated 
+    // with the length of number
     // e.g.
-    // input: int result = 15;
+    // parameter: int result = 15;
     // returns 0x00 0x02 0x31 0x35
-    public static byte[] encodeAnswer(int result) {
+    public static byte[] encodeAnswer(int result) throws IOException {
     	// the length of the result
     	int numDigits = String.valueOf(result).length();
     	byte[] answerLen = intToByteArray(numDigits);
-    	//System.out.println("answer length is: " + print(answerLen));
   
     	// convert result to String, and getBytes
     	String answer = String.valueOf(result);
     	byte[] answerBytes = answer.getBytes();
-    	//System.out.println("answer bytes = " + print(answerBytes));
   
     	// appending answer length and anwer itself
-    	byte[] appendAnswer= null;
-    	try {
-    		ByteArrayOutputStream output = new ByteArrayOutputStream();
-    		output.write(answerLen);
-    		output.write(answerBytes);
-    		byte[] out = output.toByteArray();
-    		//System.out.println("an entire answer = " + print(out));
-    		appendAnswer = out;;
-    	} catch (IOException e) {
-    		e.printStackTrace();
-    	}
-    	return appendAnswer;
+    	ByteArrayOutputStream appendAnswer = new ByteArrayOutputStream();
+    	appendAnswer.write(answerLen);
+    	appendAnswer.write(answerBytes);
+    	return appendAnswer.toByteArray();
     }
 
     // evaluate the arithmetic expression, such as "1+5-3"
     // after get the int result 3, encode the result to bytes array
-    public static byte[] evalExpression(String expression) {
+    public static byte[] evalExpression(String expression) throws IOException {
     	// evaluate the expression
     	String[] nums = expression.split("\\+|(?=-)");
     	int result = Arrays.stream(nums).mapToInt(Integer::parseInt).sum();
-    	//System.out.println("expression: " + expression + " = " + result);
   
     	return encodeAnswer(result);
     }
-
-    // this function will finish three things:
-    // 1. decode client request according to the protocol
-    // 2. evaluate every arithmetic expression
-    // 3. encode the result of arithmetric expression
-    public static byte[] decodeRequest(byte[] request) throws IOException{
-    	// firt two bytes store the number of expression
-    	int expressNum = byteToint(request, 0, 1);
-  
-    	int pos = 2; // starting from byte[2]
-    	int interval = 2; // using two bytes to store the length of expression
-  
-    	// response first two bytes store the number of answers
-    	ByteArrayOutputStream response = new ByteArrayOutputStream();
-    	response.write(Arrays.copyOfRange(request, 0, 2));
-  
-    	// decode each expression's length and its content
-    	for (int i=0; i<expressNum; i++) {
-    		int lenBuffer = byteToint(request, pos, pos + interval - 1);
-    		//System.out.println(lenBuffer);
-    		String expression = new String(Arrays.copyOfRange(request, pos + interval,
-    			pos + interval + lenBuffer));
-  
-    		byte[] val = evalExpression(expression);
-    		//System.out.println("encoded answer is:" + print(val));
-    		response.write(val);
-    		pos = pos + interval + lenBuffer;
-    	}
-    	return response.toByteArray();
-    }
-
+    
+    // this function will process input stream from client-socket
+    // and evaluate the received arithmetic expressions, ecode the
+    // answers to bytes array and send back to client-socket
     public static void processInputStream(InputStream in, OutputStream out)
             throws IOException {
-        byte[] buffer = new byte[2]; // store the first two bytes
-  
-        int len = -1;
-        int byteNum = 2;
-        int MAX_BUFFER_SIZE = 8; // if expression is too long, max read bytes is 16
-        while ((len = in.read(buffer, 0, byteNum)) != -1) {
-            ByteArrayOutputStream request = new ByteArrayOutputStream();
-            request.write(buffer, 0, byteNum);
+        int len = -1; 
+        // using two bytes to store int number in the protocol design
+        int reservedByte = 2;
+        int MAX_BUFFER_SIZE = 16;
+        byte[] buffer = new byte[reservedByte]; 
+
+        // use while loop to read the first two bytes 
+        while ((len = in.read(buffer, 0, reservedByte)) != -1) {
+            // use response to store the output stream
+            ByteArrayOutputStream response = new ByteArrayOutputStream();
+            response.write(buffer, 0, reservedByte);
             int expressionNum = byteToint(buffer, 0, 1);
-            //System.out.println("expressionNum is :" + expressionNum);
-  
+            byte[] lenBuffer = new byte[reservedByte];
+
             for (int i=0; i<expressionNum; i++) {
-                byte[] lenBuffer = new byte[2]; // store the length of expression
-                in.read(lenBuffer, 0, 2); // read two bytes
-                request.write(lenBuffer, 0, 2);
-  
+                // read the subsequent two bytes where store the expression length
+                in.read(lenBuffer, 0, reservedByte);
                 int expressionLen = byteToint(lenBuffer, 0, 1);
-                System.out.println("lenBuffer is: " + expressionLen);
-  
-                byte[] expBuffer = new byte[MAX_BUFFER_SIZE]; // store the expression
+                byte[] expBuffer = new byte[1024]; // store the expression content
+                // buffer reading at most 16 bytes each time
                 if (expressionLen > MAX_BUFFER_SIZE) {
+                    int count = 0;
+                    int tempLen = expressionLen;
                     // read untill expressionLen is smaller than MAX_BUFFER_SIZE
-                    while (expressionLen/MAX_BUFFER_SIZE > 0) {
-                         in.read(expBuffer, 0, MAX_BUFFER_SIZE);
-                         request.write(expBuffer, 0, MAX_BUFFER_SIZE);
-                         System.out.println("expression is: " + print(expBuffer));
-                         expressionLen -= MAX_BUFFER_SIZE;
+                    while (tempLen/MAX_BUFFER_SIZE > 0) {
+                         in.read(expBuffer, count*MAX_BUFFER_SIZE, MAX_BUFFER_SIZE);
+                         tempLen -= MAX_BUFFER_SIZE;
+                         count += 1;
                      }
                      // read the remaining part of the expression
-                     in.read(expBuffer, 0, expressionLen);
-                     System.out.println("remaining expression is: " + print(expBuffer));
-                     request.write(expBuffer, 0, expressionLen);
+                     in.read(expBuffer, count*MAX_BUFFER_SIZE, tempLen);
                 } else {
                     in.read(expBuffer, 0, expressionLen);
-                    request.write(expBuffer, 0, expressionLen);
-                    System.out.println("expression is: " + print(expBuffer));
                 }
-                System.out.println("request is: " + print(request.toByteArray()));
+    		    String expression = new String(Arrays.copyOfRange(expBuffer, 0, expressionLen));
+                System.out.println(i+1 + " expression: " + expression + " = ");
+
+    		    byte[] encodedanswer = evalExpression(expression);
+                response.write(encodedanswer);
           }
-  
-          byte[] response = decodeRequest(request.toByteArray());
-          // sending data to client
-          System.out.println("response is:" + print(response));
-          out.write(response);
+          // sending answers to client
+          out.write(response.toByteArray());
       }
   }
 
-  @Override
-  public void run() {
-    String client = String.format("[%s:%d]", clientSocket.getInetAddress(), clientSocket.getPort());
-    System.out.println(String.format("Handle client %s", client));
-    try {
-      InputStream in = clientSocket.getInputStream();
-      OutputStream out = clientSocket.getOutputStream();
+   @Override
+   public void run() {
+      String client = String.format("[%s:%d]", clientSocket.getInetAddress(), clientSocket.getPort());
+      System.out.println(String.format("Handle client %s", client));
+      try {
+         InputStream in = clientSocket.getInputStream();
+         OutputStream out = clientSocket.getOutputStream();
 
-      processInputStream(in, out);
+         // process input stream according to the protocol
+         processInputStream(in, out);
 
-      System.out.println(String.format("Bye bye %s", client));
-      clientSocket.close();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
+         System.out.println(String.format("Bye bye %s", client));
+         clientSocket.close();
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+   }
 }
